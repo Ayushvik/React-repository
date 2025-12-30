409f96e12f448f38b096e670655351c0338ba161
Ayush Vishwakarma <161769397+Ayushvik@users.noreply.github.com>
2025-12-30 22:07:31 +0000

    created a card

--- 01_start/src/App.jsx
+++ 01_start/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   
+    <div className='max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+      <img
+      className='w-full h-48 object-cover rounded-xl ' src="https://www.teacupsfull.com/cdn/shop/articles/Screenshot_2023-09-05_at_7.00.35_PM.png?v=1693987070&width=1100" alt="chai aur code" srcset="" />
+      <div className='p-4'>
+        <h2 className='text-large font-semibold text-black'>Card Title</h2>
+        <p className='text-large text-gray-500'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt ea veritatis necessitatibus quos quaerat corporis fugit asperiores sed odio, est ipsum adipisci, ratione repellendus ipsa.
+          </p>
+          <button className='my-2 bg-black border-2 p-3 rounded-xl font-semibold hover:not-even:bg-blue-600 text-slate-400'>Buy your coffee</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
